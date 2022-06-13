9fc7d78d63390a6dcdee99590333fa164c1dcbd5
beyzabostanci <beyza_bostanc@hotmail.com>
2022-06-14 00:13:25 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from "react"
+function EyesOnMe(){
+    return (
+        <div>
+            <button onFocus={() => console.log("Good!")} onBlur={()=> console.log("Hey! Eyes on me!")}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe
+//The onFocus event is called when the element (or some element inside of it) receives focus.
+//For example, it's called when the user clicks on a text input.
+
+//The onBlur event handler is called when focus has left the element (or left some element inside of it).
+//For example, it's called when the user clicks outside of a focused text input.
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
-// Code Keypad Component Here
+import React from "react"
+function Keypad(){
+    const handleInput = () => {
+        console.log("Entering password...")
+    }
+    return (
+        <div>
+            <input type="password" onChange={e => handleInput} />
+        </div>
+    )
+}
+export default Keypad
